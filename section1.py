f2b2e324e1c88ecd1028d1e496c8edd0c9047b30
NicolasThou <ncls.thou@gmail.com>
2020-02-25 15:14:30 +0100

    Test

--- section1.py
+++ section1.py
@@ -19,7 +19,7 @@ def r(p, s, u):
     ======
     return an integer
     
-    next_state = f(p, s, u)
+    next_state = f(p, s, u) # use of the dynamic of the problem
     p_suivant = next_state[0]
     s_suivant = next_state[1]
     if p_suivant < -1 or np.abs(s_suivant) > 3:
@@ -30,6 +30,21 @@ def r(p, s, u):
         return 0
 
 """
+
+
+def initial_state():
+    """
+    Initialization of the state with p0 uniform distribution
+    between -0.1 and 0.1 and s0 always equal to 0
+
+    return:
+    ======
+    The state [p0, s0] which is an array
+    """
+    p0 = (np.random.random_integers(-1, 1)) / 10
+    s0 = 0
+    return np.array([p0, s0])
+
 
 if __name__ == '__main__':
     print('Hello World !')
