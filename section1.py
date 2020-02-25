a41da2dd2a7cdaa1b3671693f5a2b2971d77575c
NicolasThou <ncls.thou@gmail.com>
2020-02-25 16:50:19 +0100

    comment

--- section1.py
+++ section1.py
@@ -7,9 +7,37 @@ gamma = 0.95
 delta_t = 0.001  # discretization time
 integration_step = 0.001  # euler method step
 
-"""
-def r(p, s, u):
-    
+
+def f(x, u):
+    """
+    dynamic of the system
+    """
+
+    # initialization
+    p, s = x
+    new_p = p
+    new_s = s
+
+    # check which Hill function to use
+    if p < 0:
+        g = lambda g_p, g_s, g_u: derivee_seconde_p_inf_0(g_p, g_s, g_u)
+    else:
+        g = lambda g_p, g_s, g_u: derivee_seconde_p_sup_equal_0(g_p, g_s, g_u)
+
+    # euler method with 1000 step
+    for i in range(1000):
+        temp_p = new_p
+        temp_s = new_s
+
+        # we use the values of the previous step, not the new ones !!
+        new_p += integration_step * temp_s
+        new_s += integration_step * g(temp_p, temp_s, u)
+
+    return new_p, new_s
+
+
+def r(x, u):
+    """
     reward function
     Argument:
     ========
@@ -19,18 +47,17 @@ def r(p, s, u):
     return:
     ======
     return an integer
+    """
     
     next_state = f(x, u) # use of the dynamic of the problem
     p_suivant = next_state[0]
     s_suivant = next_state[1]
     if p_suivant < -1 or np.abs(s_suivant) > 3:
         return -3
-    else if p_suivant > 1 and np.abs(s_suivant) <= 3:
+    elif p_suivant > 1 and np.abs(s_suivant) <= 3:
         return 3
     else:
         return 0
-
-"""
 
 
 def initial_state():
@@ -42,7 +69,7 @@ def initial_state():
     ======
     The state [p0, s0] which is an array
     """
-    p0 = (np.random.random_integers(-1, 1)) / 10
+    p0 = (np.random.randint(-1, 1)) / 10
     s0 = 0
     return np.array([p0, s0])
 
@@ -131,8 +158,9 @@ def random_policy():
     ======
     return an action
     """
-    power = np.random.random_integers(0, 1)
+    power = np.random.randint(0, 1)
     return ((-1) ** power) * 4
+
 
 def simulation_section2():
     """
@@ -145,6 +173,10 @@ def simulation_section2():
         action = random_policy()  # use a random policy
         state = f(state, action)  # use the dynamic of the domain
         print(state)
+        if is_final_state(state[0], state[1]) == True:
+            print('Nous avons atteint un état finale')
+            return None
+
 
 if __name__ == '__main__':
-    print('Hello World !')
+    simulation_section2()
