bc3b46ab50fe22627911cd1e6c810294062813da
NicolasThou <ncls.thou@gmail.com>
2020-02-25 15:03:16 +0100

    Test

--- section1.py
+++ section1.py
@@ -6,33 +6,26 @@ g = 9.81
 
 """
 def r(p, s, u):
-    """
-reward
-function
-Argument:
-== == == ==
-p is the
-position
-s
-the
-speed
-u
-the
-action
-return:
-== == ==
-return an
-integer
-"""
-next_state = f(p, s, u)
-p_suivant = next_state[0]
-s_suivant = next_state[1]
-if p_suivant < -1 or np.abs(s_suivant) > 3:
-    return -3
-else if p_suivant > 1 and np.abs(s_suivant) <= 3:
-    return 3
-else:
-    return 0
+    
+    reward function
+    Argument:
+    ========
+    p is the position
+    s the speed
+    u the action
+    return:
+    ======
+    return an integer
+    
+    next_state = f(p, s, u)
+    p_suivant = next_state[0]
+    s_suivant = next_state[1]
+    if p_suivant < -1 or np.abs(s_suivant) > 3:
+        return -3
+    else if p_suivant > 1 and np.abs(s_suivant) <= 3:
+        return 3
+    else:
+        return 0
 
 """
 
