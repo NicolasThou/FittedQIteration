172cd14939b0c70aa2cf1f1f72fc6b4740325975
NicolasThou <ncls.thou@gmail.com>
2020-02-28 15:10:32 +0100

    dh et ddh

--- Nicolas.py
+++ Nicolas.py
@@ -1,42 +1,179 @@
 import numpy as np
-from section1 import *
+
 
 U = np.array([-4, 4])  # action space
 m = 1
 g = 9.81
 gamma = 0.95
-delta_t = 0.001
+delta_t = 0.1  # discretization time
+integration_step = 0.001  # euler method step
 
 
-def derivee_seconde_p(p, s, u):
+def f(x, u):
     """
-    derivative 2th of p when p >= 0
+    dynamic of the system
+    """
 
+    # initialization
+    p, s = x[0], x[1]
+    new_p = p
+    new_s = s
+
+    # euler method with 1000 step
+    for i in range(1000):
+        previous_p = new_p  # we keep the previous value
+        previous_s = new_s  # we keep the previous value
+
+        # we use the values of the previous step, not the new ones !!
+        new_p = previous_p + integration_step * previous_s
+        new_s = previous_s + integration_step * f_s(previous_p, previous_s, u)
+
+    return np.array([new_p, new_s])
+
+
+def f_s(p, s, u):
+    if p < 0:
+        dH = 2 * p + 1
+        ddH = 2
+    else:
+        # recurrent term
+        k = 1 + 5 * (p ** 2)
+        # Hill'(p)
+        dH = (1/np.sqrt(k)) * (1 - ((5 * (p**2)) / k))
+        # Hill''(p)
+        ddH = (- (5 * p) / k) * ((1 / np.sqrt(k)) * (1 - ((5 * (p **2)) / k) - ((10 * (p **2)) / np.sqrt(k))) + 2)
+
+    first_term = u / (m * (1 + dH ** 2))
+    second_term = -(g * dH) / (1 + dH ** 2)
+    third_term = -((s ** 2) * dH * ddH) / (1 + dH ** 2)
+
+    return first_term + second_term + third_term
+
+
+def r(x, u):
+    """
+    reward function
     Argument:
     ========
     p is the position
     s the speed
     u the action
+    return:
+    ======
+    return an integer
+    """
+
+    next_state = f(x, u)  # use of the dynamic of the problem
+    p_suivant = next_state[0]
+    s_suivant = next_state[1]
+    if p_suivant < -1 or np.abs(s_suivant) > 3:
+        return -1
+    elif p_suivant > 1 and np.abs(s_suivant) <= 3:
+        return 1
+    else:
+        return 0
+
+
+def initial_state():
+    """
+    Initialization of the state with p0 uniform distribution
+    between -0.1 and 0.1 and s0 always equal to 0
 
     return:
     ======
-    return the value of p"
+    The state [p0, s0] which is an array
     """
-    denominateur_commun = 1 + ((s ** 2) / (1 + 5 * (p ** 2))) * ((1 - ((5 * (p ** 2)) / (1 + 5 * (p ** 2)))) ** 2)
-    numerateur1 = u / m
-    numerateur2 = g * (s / (np.sqrt(1 + 5 * (p ** 2)))) * (1 - ((5 * (p ** 2)) / (1 + 5 * (p ** 2))))
-    numerateur3 = ((15 * (s ** 5) * p) / (1 + 5 * (p ** 2))) * (
-                ((1 / (np.sqrt((1 + 5 * (p ** 2))))) * (1 - ((5 * (p ** 2)) / (1 + 5 * (p ** 2))))) ** 2)
-    denominateur_commun2 = 1 + (
-                ((1 / (np.sqrt((1 + 5 * (p ** 2))))) * (1 - ((5 * (p ** 2)) / (1 + 5 * (p ** 2))))) ** 2) * (s ** 3)
+    p0 = np.random.uniform(-0.1, 0.1)
+    s0 = 0
+    return np.array([p0, s0])
 
-    if denominateur_commun == 0 or denominateur_commun2 == 0:
-        # error to handle in the future
-        print('Denominator equal zero !!')
-        return None
 
-    return ((numerateur1 + numerateur2 + numerateur3) / denominateur_commun) / denominateur_commun2
+def is_final_state(x):
+    """
+    Check whether the system is in a final state or not
+
+    Argument:
+    =======
+    p the position
+    s the speed
+
+    Return:
+    ======
+    return a boolean
+    """
+    p, s = x
+    if abs(p) > 1 or abs(s) > 3:
+        return True
+    else:
+        return False
+
+
+def random_policy():
+    """
+    return, in a randomly way, 4 or -4
+
+    Return:
+    ======
+    return an action
+    """
+    power = np.random.randint(0, 2)
+    return ((-1) ** power) * 4
+
+
+def policy_alternative(action):
+    """
+    take the previous action, if the previous action was accelerate, then the policy
+    return -4, but if the previous action was to slow down, then return +4
+    """
+    if action == 4:  # we change the action, we accelerate then slow down etc...
+        return -4
+    else:
+        return 4
+
+
+def simulation_section2():
+    """
+    Simulate the policy in the domain from an initial state and display the trajectory
+    """
+    state = initial_state()
+    print(state)
+    a = []
+    for i in range(50):
+        action = random_policy()  # use a random policy
+        a.append(action)
+        state = f(state, action)  # use the dynamic of the domain
+        print(state)
+        if is_final_state(state) == True:
+            print(a)
+            print('Nous avons atteint un état finale')
+            return None
+    print(a)
+
+
+def simulation_section2_2():
+    """
+    Simulate the policy in the domain from an initial state and display the trajectory
+    """
+    state = initial_state()
+    print(state)
+    action = -4  # we begin with an acceleration
+    for i in range(50):
+        action = policy_alternative(action)
+        state = f(state, action)  # use the dynamic of the domain
+        print(state)
+        if is_final_state(state) == True:
+            print('Nous avons atteint un état finale')
+            return None
 
 
 if __name__ == '__main__':
-    print('Hello World !')
+
+    print('Use of random policy')
+    print('If we accelerate two times, the car is too fast, so we reach a final state')
+    simulation_section2()
+    print()
+    print('Use of alternative policy')
+    print('If we change between accelerate and slow down each time, we won\'t reach a final state')
+    simulation_section2_2()
+    print()
+    print("test")
