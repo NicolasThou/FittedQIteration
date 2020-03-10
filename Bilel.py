e4ce7a8b960326f7aa1092841e6161b6f2a390ee
BilGuet <b.guetarni@laposte.net>
2020-03-10 15:42:27 +0100

    added the trajectory generation strategies

--- Bilel.py
+++ Bilel.py
@@ -1,39 +1,25 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import Section2 as s2
+import trajectory
+import domain
 
 
-def forward_policy(x):
-    return 4
-
-
-def backward_policy(x):
-    return -4
-
-
-def hill(p):
-    if p < 0:
-        return (p**2) + p
-    else:
-        return p/(np.sqrt(1 + 5*(p**2)))
-
-
-def simulation_section2():
+def simulation():
     """
     Simulate the policy in the domain from an initial state and display the trajectory
     """
-    state = initial_state()
+    state = domain.initial_state()
     print(state[0])
     p = []
     s = []
     for i in range(50):
         action = forward_policy(state)  # use a random policy
         print(action)
-        state = f(state, action)  # use the dynamic of the domain
+        state = domain.f(state, action)  # use the dynamic of the domain
         print(state[0])
         p.append(state[0])
         s.append(state[1])
-        if is_final_state(state):
+        if domain.is_final_state(state):
             print('Nous avons atteint un état finale')
             break
     fig, axis = plt.subplots()
@@ -45,45 +31,5 @@ def simulation_section2():
     return p, s
 
 
-def disp_hill():
-    x = []
-    y = []
-    dy = []
-    ddy = []
-    for p in range(-100, 100, 1):
-        pos = (p*1.0)/100
-        x.append(pos)
-        y.append(hill(pos))
-
-        k = 1 + 5 * (pos ** 2)
-        dH = 1 / (np.sqrt(k) ** 3)
-        ddH = -(15*pos*np.sqrt(k))/(k**3)
-
-        dy.append(dH)
-        ddy.append(ddH)
-
-    fig, axis = plt.subplots(3, 1)
-    axis[0].plot(x, y, '+')
-    axis[1].plot(x, dy, '+')
-    axis[2].plot(x, ddy, '+')
-    axis[0].set(xlim=(-1, 1))
-    axis[1].set(xlim=(-1, 1))
-    axis[2].set(xlim=(-1, 1))
-    plt.show()
-
-
-def plot_hill_trajectory(p):
-    h = []
-    for x in p:
-        h.append(hill(x))
-    fig, axis = plt.subplots()
-    axis.plot(p, h, '.')
-    axis.set(xlim=(-1, 1), ylim=(-0.5, 0.5))
-    plt.show()
-
-
 if __name__ == '__main__':
-    assert is_final_state(np.array([-2, 0]))
-    assert is_final_state(np.array([0, 5]))
-
-    p, _ = simulation_section2()
+    pass
---
+++ domain.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+m = 1
+g = 9.81
+gamma = 0.95
+delta_t = 0.1  # discretization time
+integration_step = 0.001  # euler method step
+
+
+def r(x, u):
+    """
+    reward function
+    Argument:
+    ========
+    p is the position
+    s the speed
+    u the action
+    return:
+    ======
+    return an integer
+    """
+
+    p, s = f(x, u)
+
+    if p < -1 or np.abs(s) > 3:
+        return -1
+    elif p > 1 and np.abs(s) <= 3:
+        return 1
+    else:
+        return 0
+
+
+def initial_state():
+    """
+    Initialization of the state with p0 uniform distribution
+    between -0.1 and 0.1 and s0 always equal to 0
+
+    return:
+    ======
+    The state [p0, s0] which is an array
+    """
+    p0 = round(np.random.uniform(-0.1, 0.1), 2)
+    s0 = round(0, 2)
+    return np.array([p0, s0])
+
+
+def f(x, u):
+    """
+    dynamic of the system
+    """
+
+    # initialization
+    p, s = x[0], x[1]
+
+    # euler method with 1000 step
+    for i in range(1000):
+        previous_p = p  # we keep the previous value
+        previous_s = s  # we keep the previous value
+
+        # we use the values of the previous step, not the new ones !!
+        p = previous_p + integration_step * previous_s
+        s = previous_s + integration_step * f_s(previous_p, previous_s, u)
+
+    p, s = round(p, 2), round(s, 2)
+    return np.array([p, s])
+
+
+def f_s(p, s, u):
+    if p < 0:
+        # first and second derivatives of Hill(p)
+        dH = 2 * p + 1
+        ddH = 2
+    else:
+        # recurrent term
+        k = 1 + 5 * (p ** 2)
+
+        # Hill'(p)
+        dH = 1 / (np.sqrt(k) ** 3)
+        # Hill''(p)
+        ddH = -(15 * p * np.sqrt(k)) / (k ** 3)
+
+    first_term = u / (m * (1 + dH ** 2))
+    second_term = -(g * dH) / (1 + dH ** 2)
+    third_term = -((s ** 2) * dH * ddH) / (1 + dH ** 2)
+
+    return first_term + second_term + third_term
+
+
+def is_final_state(x):
+    """
+    Check whether the system is in a final state or not
+
+    Argument:
+    =======
+    p the position
+    s the speed
+
+    Return:
+    ======
+    return a boolean
+    """
+    p, s = x
+    if abs(p) > 1 or abs(s) > 3:
+        return True
+    else:
+        return False
