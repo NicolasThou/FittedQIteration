62f7a230f2b95b898b2a767706d1ed8bdb298fc3
BilGuet <b.guetarni@laposte.net>
2020-03-19 14:59:09 +0100

    Added training and test

--- FittedQIteration.py
+++ FittedQIteration.py
@@ -1,33 +1,78 @@
 import ScikitExtraTree as SET
+import ScikitLinearRegression as SLR
 import numpy as np
+from matplotlib import pyplot as plt
 import section5 as S5
 import domain
 
 
 if __name__ == '__main__':
     # trajectory
-    trajectory = S5.first_generation_set_one_step_system_transition(500)
+    trajectory = [S5.first_generation_set_one_step_system_transition(500),
+                  S5.second_generation_set_one_step_system_transition(500)]
 
-    # horizon of Q
-    q_N = 3
+    plt.subplot(2, 2)
+    for number, h in enumerate(trajectory):
 
-    # Q functions
-    Q = []
+        # horizon of Q
+        q_N = 5
 
-    # iteration N=1
-    X, Y = S5.build_training_set(trajectory, None, 0)
-    Q.append(SET.SkExtraTree(n_trees=50, n_min=2))
-    Q[0].fit(X, Y)
+        # Q functions
+        Q_tree = []
+        Q_reg = []
 
-    # iteration N>1
-    for i in range(1, q_N):
-        X, Y = S5.build_training_set(trajectory, Q[i-1], i)
-        Q.append(SET.SkExtraTree(n_trees=50, n_min=2))
-        Q[i].fit(X, Y)
+        """
+            TRAINING
+        """
+        # iteration N=1
+        X, Y = S5.build_training_set(h, None, 0)
 
-    p, s = domain.initial_state()
-    u = -4
-    r = Q[-1].predict([[p, s, u]])
+        # add new models
+        Q_tree.append(SET.SkExtraTree(n_trees=50, n_min=2))
+        Q_reg.append(SLR.SkLinearRegression())
 
-    print("{} {}".format(p, s))
-    print(r)
+        # train the models
+        Q_tree[0].fit(X, Y)
+        Q_reg[0].fit(X, Y)
+
+        # iteration N>1
+        for i in range(1, q_N):
+            X_tree, Y_tree = S5.build_training_set(h, Q_tree[i-1], i)
+            X_reg, Y_reg = S5.build_training_set(h, Q_reg[i-1], i)
+
+            Q_tree.append(SET.SkExtraTree(n_trees=50, n_min=2))
+            Q_reg.append(SLR.SkLinearRegression())
+
+            Q_tree[i].fit(X_tree, Y_tree)
+            Q_reg[i].fit(X_reg, Y_reg)
+
+        """
+            TEST
+        """
+        tree_error = []
+        reg_error = []
+
+        n = range(q_N)
+        for i in n:
+            tree_n_error = []
+            reg_n_error = []
+
+            for k in range(50):
+                tuple = S5.second_generation_set_one_step_system_transition(1)[0]
+                p, s = tuple[0]
+                u = tuple[1]
+
+                tree_n_error.append(abs(Q_tree[i].predict([[p, s, u]]) - Q_tree[i+1].predict([[p, s, u]])))
+                reg_n_error.append(abs(Q_reg[i].predict([[p, s, u]]) - Q_reg[i+1].predict([[p, s, u]])))
+
+            tree_error.append(np.mean(tree_n_error))
+            reg_error.append(np.mean(reg_n_error))
+
+        plt.subplot(number+1, 1)
+        plt.plot(n, tree_error)
+
+        plt.subplot(number+1, 2)
+        plt.plot(n, reg_error)
+
+    plt.show()
+
--- ScikitLinearRegression.py
+++ ScikitLinearRegression.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     X = [[1, 2, 3], [3, 4, 5], [2, 4, 5]]
     Y = [6, 12, 11]
 
-    reg = SkLinearRegression().fit(X, Y)
+    reg = SkLinearRegression()
+    reg.fit(X, Y)
 
     print(reg([[2, 7, 9]]))
 
